ad38713c0e8e038ee75562f9af8a3f03158e6555
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-04-27 19:46:26 +0545

    chore: set up TailwindCSS with PostCSS configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AllUsersComponent } from './CRUD/Pages/all-users/all-users.component';
+import { CreateUserComponent } from './CRUD/Pages/create-user/create-user.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AllUsersComponent, CreateUserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
